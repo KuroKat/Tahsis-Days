6bbe522a2b751f5862fa1d3af7795d1c41f4a3dc
KuroKat <demonsrcute@gmail.com>
2026-07-05 08:52:38 -0700

    modified:   data/stations.json 	modified:   js/stations.js 	modified:   manifest.json

--- js/stations.js
+++ js/stations.js
@@ -1,11 +1,16 @@
-const stations={
-
-RANGER001:10,
-
-RANGER002:15,
-
-RANGER003:20,
-
-BONUS001:50
-
-};
+[
+  {
+    "id": "RANGER001",
+    "name": "Fire Starting",
+    "points": 20,
+    "description": "Build and light a fire using one match.",
+    "type": "skill"
+  },
+  {
+    "id": "RANGER002",
+    "name": "Navigation",
+    "points": 15,
+    "description": "Find the next bearing.",
+    "type": "navigation"
+  }
+]
